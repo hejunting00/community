c164b28de102ad3e7e04a96a6f47921bbaafd8e2
Junting He <1215076525@qq.com>
2021-11-04 00:01:49 +0800

    The sixth day

--- src/main/java/com/example/springboottest/dto/GithubUser.java
+++ src/main/java/com/example/springboottest/dto/GithubUser.java
@@ -2,7 +2,7 @@ package com.example.springboottest.dto;
 
 public class GithubUser {
     private String name;
-    private long id;
+    private String id;
     private String bio;
 
     public String getName() {
@@ -13,11 +13,11 @@ public class GithubUser {
         this.name = name;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
---
+++ src/main/java/com/example/springboottest/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package com.example.springboottest.mapper;
+
+import com.example.springboottest.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator})")
+    void create(Question question);
+}
+
